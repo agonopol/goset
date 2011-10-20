16e26e4a9c02cf4c58b0a844ee6b1ba3b8d2f402
Alex Gonopolskiy <agonopol@gmail.com>
2011-10-20 09:13:28 -0500

    shie

--- set.go
+++ set.go
@@ -27,6 +27,25 @@ func (this *Set) Has(x interface{}) bool {
 	return found
 }
 
+func (this *Set) Do(f func (interface{})) {
+	for k,_ := range *this {
+		f(k)
+	}
+}
+
+func (this *Set) Len() int {
+	return len(*this)
+}
+
+func (this *Set) Map(f func(interface{}) interface{}) []interface{} {
+	set := make([]interface{}, len(*this))
+	i := 0
+	for k,_ := range *this {
+		set[i] = f(k)
+	}
+	return set
+}
+
 func (this *Set) MarshalJSON() ([]byte, os.Error) {
 	set := make([]interface{}, 0)
 	for k, _ := range *this {
